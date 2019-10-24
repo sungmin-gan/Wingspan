08e282bf42c767d6f4d2b0e3bf880f5477babe94
Armand0alexis <39757452+Armand0alexis@users.noreply.github.com>
2019-10-23 22:22:39 -0700

    Add files via upload

---
+++ testBirdCards.java
@@ -0,0 +1,36 @@
+package optionsClass;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.ArrayList;
+
+import org.junit.jupiter.api.Test;
+
+class testBirdCards {
+
+	
+	ArrayList<birdClass> cards = new ArrayList<birdClass>();
+	
+	@Test
+	void birdCards() {
+		
+		birdClass newBird0 = new birdClass();
+		newBird0.setBirdHabitat("forest");
+		birdClass newBird1 = new birdClass();
+		newBird1.setBirdHabitat("wet lands");
+		birdClass newBird2 = new birdClass();
+		newBird2.setBirdHabitat("dry lands");
+		
+		cards.add(newBird0);
+		cards.add(newBird1);
+		cards.add(newBird2);
+		
+		assertEquals("forest", cards.get(0).getBirdHabitat());
+		assertEquals("wet lands", cards.get(1).getBirdHabitat());
+		assertEquals("dry lands",cards.get(2).getBirdHabitat());
+		
+		
+	}
+	
+	
+}
