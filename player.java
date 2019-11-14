b42d959b0744f2171b5c91c004a346ae7a3968e3
sungmin-gan <gan.sungmin@gmail.com>
2019-11-13 17:41:27 -0800

    Create player.java
    This should replace the previous player.java file. Still not complete.

---
+++ player.java
@@ -0,0 +1,70 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Player {
+	String name;
+	int invertebrateTokens, seedTokens, fishTokens, fruitTokens, rodentTokens, eggs;
+	ArrayList<BirdCard> birdCards = new ArrayList<BirdCard>();
+	ArrayList<BonusCard> bonusCards = new ArrayList<BonusCard>();
+	
+	Player(String _name){
+		name = _name;
+	}
+	
+	public void addFoodToken(FoodType _foodType) {
+		switch(_foodType) {
+		case invertebrate:
+			invertebrateTokens += 1;
+			break;
+		case seed:
+			seedTokens += 1;
+			break;
+		case fish:
+			fishTokens += 1;
+			break;
+		case fruit:
+			fruitTokens += 1;
+			break;
+		case rodent:
+			rodentTokens += 1;
+			break;
+		}
+	}
+	
+	public void spendFoodToken(FoodType _foodType) {
+		switch(_foodType) {
+		case invertebrate:
+			invertebrateTokens -= 1;
+			break;
+		case seed:
+			seedTokens -= 1;
+			break;
+		case fish:
+			fishTokens -= 1;
+			break;
+		case fruit:
+			fruitTokens -= 1;
+			break;
+		case rodent:
+			rodentTokens -= 1;
+			break;
+		}
+	}
+	
+	public void addBonusCard(BonusCard _bonusCard) { bonusCards.add(_bonusCard); }
+	public void removeBonusCard(int index) { bonusCards.remove(index); }
+	public BonusCard getBonusCard(int index) { return bonusCards.get(index); }
+	
+	public void addEggs(int x) {
+		for (int i = 0; i < x; i++) {
+			eggs += 1; 
+		}
+	}
+	public void subtractEggs(int x) {
+		for (int i = 0; i < x; i++) {
+			eggs -= 1; 
+		}
+	}
+	
+}
